eec33ee596b3afe7307fbd4851a6d77891c3f68c
mrk <mpruszkowski11@gmail.com>
2023-10-10 21:18:51 +0200

    added simple ui and reset functionality

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let playerSelection = null;
 let round = 1;
 let playerScore = 0;
 let computerScore = 0;
+
+// listen to buttons clicks and play a round with the player's choice
 
 const rock = document.querySelector('#rock');
 rock.addEventListener('click', () => {
@@ -18,17 +19,31 @@ scissors.addEventListener('click', () => {
     playRound('scissors', getComputerChoice());
 });
 
+// some containers for the results
+
 const resultContainer = document.querySelector('#result');
 var resultText = document.createElement('p');
 
-const scoreboard = document.querySelector('#score');
+const finalResult = document.createElement('p');
+
+const scoreboard = document.querySelector('#scoreboard');
 var scoreText = document.createElement('p');
+
+// reset button
+const resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click', () => {
+    reset();
+});
+
+// get a random choice for the computer
 
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];   
 };
+
+// play a round and update the result of the round
 
 function playRound(playerSelection, computerSelection) {
 
@@ -60,29 +75,20 @@ function playRound(playerSelection, computerSelection) {
             resultContainer.appendChild(resultText);
         }
     }
+
+    game();
 }
+
+// keep track of the score and display the final result
 
 function game() {
 
-    const buttons = document.querySelectorAll('button');
-
-    buttons.forEach((button) => {
-        button.addEventListener(('click'), () => {
-            score();
-            console.log(playerSelection);
-        });
-    });
-}
-
-function score() {
-
     let roundResult;
-    let computerSelection;
     let result;
-    let roundText = document.createElement('p');
+    let scoreText = document.createElement('p');
+    
 
     if (playerScore < 5 && computerScore < 5) {
-        computerSelection = getComputerChoice();
         roundResult = resultText.textContent;
         if (roundResult.includes('win')) {
             playerScore++;
@@ -91,23 +97,34 @@ function score() {
         }
         result = `Round ${round}: ${roundResult} Score: ${playerScore}-${computerScore}`;
         scoreText.textContent = (result);
-        scoreboard.appendChild(scoreText);
-        console.log(result);
+        scoreboard.insertBefore(scoreText, scoreboard.firstChild);
         round++;
-    } else {
-
-        resultText.textContent = ('Game over!');
-
-        if (playerScore > computerScore) {
-            console.log('You win!');
-        } else if (computerScore > playerScore) {
-            console.log('You lose!');
-        } else {
-            console.log('Tie!');
-        }
-    
     }
 
+    if (playerScore === 5 || computerScore === 5) {
+        resultText.textContent = ('Game over!');
+        resultContainer.appendChild(resultText);
+
+        if (playerScore > computerScore) {
+            finalResult.textContent = ('You win!');
+        } else if (computerScore > playerScore) {
+            finalResult.textContent = ('You lose!');
+        } else {
+            finalResult.textContent = ('Tie!');
+        }
+        resultContainer.appendChild(finalResult);
+
+        resetButton.classList.remove('hidden');
+
+    } 
 }
 
-game();
+function reset() {
+    round = 1;
+    playerScore = 0;
+    computerScore = 0;
+    resultText.textContent = ('');
+    finalResult.textContent = ('');
+    scoreboard.textContent = ('');
+    resetButton.classList.add('hidden');
+}
